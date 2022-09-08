c3891fa216a90ed32ff562d35616dde53356f543
Sibyl <metinkorkmaz417@gmail.com>
2022-09-08 16:19:18 +0300

    update placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-</script>
 
 <template>
-  <p class="text-center text-xl">osu! GST will be here!</p>
+  <img class="mx-auto" src="https://cdn.discordapp.com/attachments/1017154849613287467/1017423328077688873/FcCXPCsaQAA90WN.jpg" alt="visibly shaking">
 </template>
