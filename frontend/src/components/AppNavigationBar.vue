793e7ce586ec4acbd8fcd49b05c80525b3ef0d30
Sibyl <metinkorkmaz417@gmail.com>
2022-09-15 22:21:04 +0300

    little design change to navigation hover menu

--- frontend/src/components/AppNavigationBar.vue
+++ frontend/src/components/AppNavigationBar.vue
@@ -14,10 +14,10 @@ const routes = router.options.routes.filter(x => x.name);
     <template v-for="route in routes">
       <router-link class="nav-item nav-route" v-if="!route.children" :to="route.path">{{ route.name }}</router-link>
       <div v-else class="group relative">
-        <p class="nav-item nav-route group-hover:rounded-b-none">{{ route.name }}</p>
-        <div class="absolute group-visible bg-white w-full h-5"></div>
+        <p class="nav-item nav-route">{{ route.name }}</p>
+        <div class="absolute group-visible w-full h-5"></div>
 
-        <div class="absolute top-10 group-visible flex flex-col rounded-without-tl py-1 bg-white">
+        <div class="absolute top-10 group-visible flex flex-col rounded py-1 bg-white">
           <router-link v-for="childrenRoute in route.children" :key="childrenRoute.path"
             class="nav-item nav-route-child" :to="`${route.path}/${childrenRoute.path}`">
             {{ childrenRoute.name }}
