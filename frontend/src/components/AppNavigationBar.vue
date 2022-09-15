2eb47a2e915ea8abd1f54f44de4418aa40940717
Sibyl <metinkorkmaz417@gmail.com>
2022-09-15 23:33:56 +0300

    overhaul navigation bar

--- frontend/src/components/AppNavigationBar.vue
+++ frontend/src/components/AppNavigationBar.vue
@@ -6,24 +6,24 @@ const routes = router.options.routes.filter(x => x.name);
 </script>
 
 <template>
-  <div class="flex justify-center items-center gap-8 bg-black text-white p-3">
+<div class="bg-black text-white p-3 h-16 flex items-center justify-between lg:justify-center">
     <router-link to="/">
-      <img src="../assets/gstlive.png" />
+      <img src="../assets/gstlive.png" alt="gstlive icon" class="h-7 mr-16" />
     </router-link>
 
-    <template v-for="route in routes">
-      <router-link class="nav-item nav-route" v-if="!route.children" :to="route.path">{{ route.name }}</router-link>
-      <div v-else class="group relative">
-        <p class="nav-item nav-route">{{ route.name }}</p>
-        <div class="absolute group-visible w-full h-5"></div>
+    <img class="lg:hidden ml-auto" src="../assets/menu.svg" alt="menu button" />
 
-        <div class="absolute top-10 group-visible flex flex-col rounded py-1 group-opacity-transition bg-white">
-          <router-link v-for="childrenRoute in route.children" :key="childrenRoute.path"
-            class="nav-item nav-route-child" :to="`${route.path}/${childrenRoute.path}`">
-            {{ childrenRoute.name }}
-          </router-link>
+    <div class="hidden lg:flex justify-center max-auto gap-8">
+      <div v-for="route in routes" class="relative group">
+        <router-link class="nav-item nav-route" :to="route.path">{{ route.name }}</router-link>
+
+        <div class="absolute group-visible w-full h-5"></div>
+        <div class="absolute flex flex-col top-10 -left-1/4 py-2 bg-white rounded group-opacity-transition group-visible">
+          <template v-for="childRoute in route.children">
+            <router-link class="nav-item nav-route-child text-center" :to="`${route.path}/${childRoute.path}`">{{ childRoute.name }}</router-link>
+          </template>
         </div>
       </div>
-    </template>
+    </div>
   </div>
 </template>
