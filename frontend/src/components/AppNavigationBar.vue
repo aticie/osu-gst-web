cce07c58e82a26ca8a4af6c7bcc2836dbad72c7d
Sibyl <metinkorkmaz417@gmail.com>
2022-09-17 16:52:33 +0300

    mobile support to navigation bar

--- frontend/src/components/AppNavigationBar.vue
+++ frontend/src/components/AppNavigationBar.vue
@@ -1,28 +1,43 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
+const isMenuOpen = ref(false);
 const router = useRouter();
 const routes = router.options.routes.filter(x => x.name);
+
+const toggleMenu = () => isMenuOpen.value = !isMenuOpen.value;
 </script>
 
 <template>
-<div class="bg-black text-white p-3 h-16 flex items-center justify-between lg:justify-center">
-    <router-link to="/">
-      <img src="../assets/gstlive.png" alt="gstlive icon" class="h-7 mr-16" />
-    </router-link>
+  <div class="bg-black p-3 text-white flex flex-col lg:flex-row lg:justify-center lg:items-center">
+    <div class="flex items-center justify-between">
+      <router-link to="/">
+        <img src="../assets/gstlive.png" alt="gstlive icon" class="lg:mr-10" />
+      </router-link>
 
-    <img class="lg:hidden ml-auto" src="../assets/menu.svg" alt="menu button" />
+      <img @click="toggleMenu" class="lg:hidden" src="../assets/menu.svg" alt="menu button" />
+    </div>
 
-    <div class="hidden lg:flex justify-center max-auto gap-8">
+    <div 
+      class="flex flex-col lg:flex-row lg:max-h-96 gap-2 marker:lg:gap-8 text-center overflow-hidden lg:overflow-visible transition-all  max-h-0"
+      :class="{ 'max-h-96': isMenuOpen }"
+    >
+      <div class="mt-2"></div>  
+
       <div v-for="route in routes" class="relative group">
         <router-link class="nav-item nav-route" :to="route.path">{{ route.name }}</router-link>
 
         <template v-if="route.children">
-          <div class="absolute group-visible w-full h-5"></div>
-          <div class="absolute flex flex-col top-10 -left-1/4 py-2 bg-white rounded group-opacity-transition group-visible">
-            <template v-for="childRoute in route.children">
-              <router-link class="nav-item nav-route-child text-center" :to="`${route.path}/${childRoute.path}`">{{ childRoute.name }}</router-link>
-            </template>
+          <div class="absolute h-2 w-full group"></div>
+
+          <div class="overflow-hidden lg:visible max-h-0 group-hover:max-h-96 transition-all">
+            <div class="py-2 bg-white flex flex-wrap justify-center mt-1 lg:absolute group-visible rounded">
+              <router-link class="nav-item nav-route-child rounded lg:rounded-none lg:w-full" v-for="childRoute in route.children"
+                :to="`${route.path}/${childRoute.path}`">
+                {{ childRoute.name }}
+              </router-link>
+            </div>
           </div>
         </template>
       </div>
