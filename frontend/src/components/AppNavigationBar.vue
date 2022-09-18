f9bfc010a90b37e366dcefc7116c3f0c18d7dce8
Sibyl <metinkorkmaz417@gmail.com>
2022-09-18 23:40:13 +0300

    update mobile version of navigation bar

--- frontend/src/components/AppNavigationBar.vue
+++ frontend/src/components/AppNavigationBar.vue
@@ -11,7 +11,36 @@ const closeMenu = () => isMenuOpen.value = false;
 </script>
 
 <template>
-  <div v-click-outside="closeMenu" class="bg-black p-3 text-white flex flex-col lg:flex-row lg:justify-center lg:items-center">
+  <div v-click-outside="closeMenu"
+    class="bg-black text-white flex items-center justify-between lg:justify-center gap-16 p-3.5">
+    <router-link to="/">
+      <img src="../assets/gstlive.png" alt="gstlive icon" class="h-6" />
+    </router-link>
+
+    <img @click="toggleMenu" src="../assets/menu.svg" alt="menu button" class="lg:hidden h-7" />
+    <div
+      class="flex flex-col lg:nav-large absolute inset-y-0 bg-black p-6 right-0 translate-x-full gap-3 transition-transform"
+      :class="{'translate-x-0': isMenuOpen}">
+      <div v-for="route in routes" class="relative group">
+        <router-link 
+          class="nav-item nav-route"
+          :class="{ 'group-hover:bg-red-500 group-hover:text-white': route.children }" 
+          :to="route.path"
+        >
+          {{ route.name }}
+        </router-link>
+
+        <div v-if="route.children" class="flex flex-col mt-2 border-l-2 border-red-500 transition-colors">
+          <router-link v-for="childRoute in route.children" :to="`${route.path}/${childRoute.path}`"
+            class="nav-item nav-route-child">
+            {{ childRoute.name }}
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div v-click-outside="closeMenu" class="bg-black p-3 text-white flex flex-col lg:flex-row lg:justify-center lg:items-center">
     <div class="flex items-center justify-between">
       <router-link to="/">
         <img src="../assets/gstlive.png" alt="gstlive icon" class="lg:mr-10 h-6" />
@@ -21,7 +50,7 @@ const closeMenu = () => isMenuOpen.value = false;
     </div>
 
     <div 
-      class="flex flex-col lg:flex-row lg:max-h-96 gap-2 lg:gap-8 text-center overflow-hidden lg:overflow-visible transition-all  max-h-0"
+      class="flex flex-col lg:flex-row lg:max-h-96 gap-2 lg:gap-8 text-center overflow-hidden lg:overflow-visible transition-all "
       :class="{ 'max-h-96': isMenuOpen }"
     >
       <div class="mt-2"></div>  
@@ -43,5 +72,5 @@ const closeMenu = () => isMenuOpen.value = false;
         </template>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
