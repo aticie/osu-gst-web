07ad94f4f615d7297252254dd65b779e22363835
Sibyl <metinkorkmaz417@gmail.com>
2022-10-27 20:18:20 +0300

    switch icons to svg

--- frontend/src/components/AppNavigationBar.vue
+++ frontend/src/components/AppNavigationBar.vue
@@ -1,32 +1,48 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 
+const isMenuOpen = ref(false);
 const route = useRoute();
 const router = useRouter();
 const routes = router.options.routes.filter(route => route.path != "/");
+
+const openMenu = () => isMenuOpen.value = true;
+const closeMenu = () => isMenuOpen.value = false;
 </script>
 
 <template>
-  <nav class="flex justify-between xl:gap-20 bg-black h-20 px-4 route-links text-2xl font-bold relative">
-    <div class="flex xl:flex-1 items-center justify-center gap-2">
-      <div class="h-5 w-5 bg-white xl:hidden" />
+  <nav 
+    class="flex justify-between xl:gap-20 bg-black h-20 px-4 route-links text-lg lg:text-2xl font-bold"
+    v-click-outside="closeMenu"
+  >
+    <div class="hidden xl:block flex-1" />
+
+    <div class="flex xl:flex-grow items-center justify-center gap-2">
+      <button class="xl:hidden w-6" @click="openMenu">
+        <img src="../assets/svgs/burger.svg" alt="burger menu" />
+      </button>
   
       <RouterLink to="/" class="h-8 flex items-center xl:mr-14" active-class="route-active">
-        <img src="../assets/gstlive.png" class="h-5" />
+        <img src="../assets/svgs/gstlive.svg" class="h-4 lg:h-5" />
       </RouterLink>
 
-      <ul class="hidden xl:flex gap-14">
+      <ul 
+        class="absolute xl:static flex flex-col xl:flex-row inset-y-0 left-0 bg-black p-4 -translate-x-full xl:translate-x-0 transition-transform gap-4 xl:gap-14"
+        :class="{ 'translate-x-0': isMenuOpen }"
+      >
         <RouterLink
           v-for="route in routes"
           :to="route.path"
+          class="flex items-center justify-center"
           active-class="route-active"
         >
-          {{ route.name }}
+          <p>{{ route.name }}</p>
         </RouterLink>
       </ul>
     </div>
 
-    <div class="flex items-center gap-2 xl:absolute xl:right-4 xl:inset-y-0">
+    <div class="flex flex-1 items-center justify-end gap-2">
       <p class="mt-1 sm:mt-0 xl:hidden">{{ route.name }}</p>
       <div class="bg-pink-accent w-1 h-4 xl:hidden" />
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,19 +3,24 @@
 
 <template>
   <div>
-    <div class="absolute p-6 inset-0 -z-10 flex items-center justify-center overflow-hidden">
-      <div class="z-10">
-        <img src="../assets/center-logo.png" alt="gstlive center page logo" />
-        <div class="mt-10 flex flex-col xl:flex-row justify-center items-center gap-6">
-          <img src="../assets/center-signup.png" alt="gstlive center page logo" />
-          <img src="../assets/center-discord.png" alt="gstlive center page logo" />
+    <div class="absolute inset-0 -z-10 flex items-center justify-center overflow-hidden">
+      <div class="z-10 p-16">
+        <img src="../assets/svgs/gstlive-with-text.svg" alt="gstlive center page logo" />
+
+        <div class="mt-10 px-10 flex flex-col xl:flex-row justify-center items-center gap-6">
+          <img src="../assets/svgs/signup.svg" alt="gstlive center page logo" />
+          <img src="../assets/svgs/discord.svg" alt="gstlive center page logo" />
         </div>
       </div>
 
-      <div class="spinning-square animate-spin-25" style="width: 500px; height: 500px; animation-delay: -15s;" />
-      <div class="spinning-square animate-spin-50" style="width: 550px; height: 550px; animation-delay: -20s;" />
-      <div class="spinning-square animate-spin-75" style="width: 500px; height: 500px; animation-delay: -25s;" />
-      <div class="spinning-square animate-spin-100" style="width: 550px; height: 550px; animation-delay: -30s;" />
+      <div 
+        v-for="num in 4"
+        class="spinning-square w-full aspect-square max-w-2xl"
+        :class="`animate-spin-${25 * num}`"
+        :style="{
+          animationDelay: `-${15 + (num * 5)}s`
+        }"
+      />
     </div>
   </div>
 </template>
