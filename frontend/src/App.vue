ff78f8ff0604aa5c645c2b210f13b5ca8a0f82e2
Sibyl <metinkorkmaz417@gmail.com>
2022-11-03 18:35:49 +0300

    comment out navigation bar to fix build error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
-import AppNavigationBar from './components/AppNavigationBar.vue';
 
 const route = useRoute();
 const isMenuOpen = ref(true);
 </script>
 
 <template>
-  <AppNavigationBar v-if="route.path !== '/'" v-model="isMenuOpen" />
+  <!-- <AppNavigationBar v-if="route.path !== '/'" v-model="isMenuOpen" /> -->
   <div class="artwork absolute inset-0" />
 
   <RouterView v-slot="{ Component }">
