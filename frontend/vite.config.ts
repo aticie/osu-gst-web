625fc90f4f77cd103868591868b7654ec295d48a
Sibyl <metinkorkmaz417@gmail.com>
2022-11-04 22:40:55 +0300

    test vite env plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 
 export default defineConfig(() => {
   return {
-    plugins: [vue()],
+    plugins: [vue(), env()],
     build: {
       outDir: "../dist"
     }
