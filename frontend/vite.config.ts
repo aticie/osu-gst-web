6981138f1c23cc8d922e37a06116ae2bf2b190e8
Sibyl <metinkorkmaz417@gmail.com>
2022-11-04 20:38:48 +0300

    let's try this

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from "fs";
-
-// https://github.com/sveltejs/kit/issues/3004#issuecomment-1280759553
-const vite_env_variables = Object.entries(process.env).filter(variable => variable[0].startsWith("VITE_"));
-const vite_env_variable_string = vite_env_variables.map(variable => variable[0] + "=" + variable[1]).join("\n");
-fs.writeFileSync(".env.local", vite_env_variable_string, {encoding: "utf-8"});
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: "../dist"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  var variables = {};
+  for (const [key, value] of Object.entries(env)) {
+    variables[key] = value;
+  }
+
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: "../dist"
+    },
+    define: variables
   }
 })
