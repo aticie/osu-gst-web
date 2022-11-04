d69cd7094e108e84052e68d8dc2dcb48c157e3fc
Sibyl <metinkorkmaz417@gmail.com>
2022-11-04 22:06:38 +0300

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from "fs";
 
 // https://vitejs.dev/config/
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_");
-
-  var variables = {};
-  for (const [key, value] of Object.entries(env)) {
-    variables[key] = value;
-  }
+export default defineConfig(() => {
+  const vite_env_variables = Object.entries(process.env).filter(variable => variable[0].startsWith("VITE_"));
+  const vite_env_variable_string = vite_env_variables.map(variable => variable[0] + "=" + variable[1]).join("\n");
+  fs.writeFileSync("/app/.env.local", vite_env_variable_string, {encoding: "utf-8"});
 
   return {
     plugins: [vue()],
     build: {
       outDir: "../dist"
-    },
-    define: variables
+    }
   }
 })
