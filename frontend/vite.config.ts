e03aacc0455ad5708c6031654b0463bec3ac4694
Sibyl <metinkorkmaz417@gmail.com>
2022-11-04 22:38:23 +0300

    idk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from "fs";
 
 // https://vitejs.dev/config/
 
 export default defineConfig(() => {
-  const vite_env_variables = Object.entries(process.env).filter(variable => variable[0].startsWith("VITE_"));
-  const vite_env_variable_string = vite_env_variables.map(variable => variable[0] + "=" + variable[1]).join("\n");
-  fs.writeFileSync("/app/frontend/.env.local", vite_env_variable_string, {encoding: "utf-8"});
-
   return {
     plugins: [vue()],
     build: {
