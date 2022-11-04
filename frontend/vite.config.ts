765ed69a5093c944050bc0feb3e1a0f0aca82f2d
Sibyl <metinkorkmaz417@gmail.com>
2022-11-04 22:56:32 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 
 export default defineConfig(() => {
   return {
-    plugins: [vue(), env()],
+    plugins: [vue()],
     build: {
       outDir: "../dist"
     }
