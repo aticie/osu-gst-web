3507900cd1fb4ecfd6fb611d423e49b356c9a933
Sibyl <metinkorkmaz417@gmail.com>
2022-11-09 19:45:22 +0300

    add animated background

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -15,6 +15,19 @@ module.exports = {
       },
       fontFamily: {
         "inter": "inter"
+      },
+      keyframes: {
+        "horizontal-loop": {
+          "from": {
+            "background-position": "0% 0%"
+          },
+          "to": {
+            "background-position": "100% 0%"
+          }
+        }
+      },
+      animation: {
+        "horizontal-loop": "horizontal-loop 80s linear infinite"
       }
     }
   },
