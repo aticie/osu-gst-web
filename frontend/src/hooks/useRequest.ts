0288ae1f067795200dfc479cdc4d30e860318006
Sibyl <metinkorkmaz417@gmail.com>
2022-11-11 23:09:43 +0300

    throw exception on status below 500

--- frontend/src/hooks/useRequest.ts
+++ frontend/src/hooks/useRequest.ts
@@ -3,7 +3,10 @@ import { useNotify } from "./useNotify";
 
 export const useRequest = async <T>(config: AxiosRequestConfig) => {
   try {
-    const response = await axios<T>(config);
+    const response = await axios<T>({
+      ...config,
+      validateStatus: status => status <= 500
+    });
     return response.data;
   } catch(error) {
     if (!axios.isAxiosError(error)) return;
