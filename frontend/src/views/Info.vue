9fba0d087b9dbd25b6c145fc56be9c799bf4bc1b
Sibyl <metinkorkmaz417@gmail.com>
2022-11-12 21:27:54 +0300

    finish info page

--- frontend/src/views/Info.vue
+++ frontend/src/views/Info.vue
@@ -319,5 +319,103 @@ import InfoStaff from '../components/info/InfoStaff.vue';
         </div>
       </div>
     </section>
+
+    <section class="info-container">
+      <div class="info-head">
+        <h1>RULESETS</h1>
+        <p>Play by the book!</p>
+      </div>
+
+      <div class="info-content">
+        <div>
+          <h1>Registration</h1>
+
+          <ol>
+            <li>
+              <p>Registration will be through the <a href="https://gstlive.org/">GST:LIVE website</a></p>
+            </li>
+            <li>
+              <p><a href="https://github.com/underscoregt/osu-wiki/tree/master/wiki/Tournaments/Badge-weighted_seeding">Badge-weighted seeding</a>(BWS) will be used with the formula of rank^(0.9937^(badges^2)).</p>
+            </li>
+            <li>
+              <p>Average team BWS rank must be #2000 or lower.</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+
+      <div class="info-content">
+        <div>
+          <h1>Qualifiers regulations</h1>
+
+          <ol>
+            <li>
+              <p>All teams will play through the Qualifiers pool, consisting of 9 maps, in order from A1 to A9.</p>
+            </li>
+            <li>
+              <p>Each team must play through all the maps; failure to do so will result in disqualification.</p>
+            </li>
+            <li>
+              <p>If a player disconnects or encounters any technical issues within the first 30 seconds of a map, they are allowed to replay that particular map after the lobby is done with all the remaining maps.</p>
+            </li>
+            <li>
+              <p>Only the top 8 teams will proceed to the Bracket Stage. Seeding in Qualifiers will be determined by the sum of z-score in each map.</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+
+      <div class="info-content">
+        <div>
+          <h1>Bracket Stage regulations</h1>
+
+          <ol>
+            <li>
+              <p>The Bracket Stage will be held offline on 17 December 2022 at AEF Online 2022. By signing up, you agree that you are free and will be showing up to the event should you place top 8 in Qualifiers. Should there be an emergency such that you are unable to make the event, please let the staff know by messaging megumic (ue#0001) or _gt (gt#8668) on Discord.</p>
+            </li>
+            <li>
+              <p>If a player is not present at the time of their match, the match will be forfeited. The staff reserves the right to ban absent players for future iterations of GST and GST:LIVE.</p>
+            </li>
+            <li>
+              <p>There will be 2 mappools: the Quarterfinals pool will be Bo7, and the Semifinals/Finals pool will be Bo9.</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+
+      <div class="info-content">
+        <div>
+          <h1>Match procedure</h1>
+
+          <ol>
+            <li>
+              <p>The higher !roll gets to choose pick order.</p>
+            </li>
+            <li>
+              <p>Each team will be given a maximum of 90 seconds for picking maps. Failure to decide before the time limit will result in the action being passed to the opponent.</p>
+            </li>
+            <li>
+              <p>For each map that does not require the Hidden mod, players may choose to play said maps with Hidden, with those scores divided by 1.06. As such, you are required to co-operate by putting on the required mods for each map as the multiplayer lobby will virtually permanently have Free Mod enabled.</p>
+            </li>
+            <li>
+              <p>The mappool is designed such that if both teams reach match point, there will be 3 maps left.</p>
+            </li>
+            <li>
+              <p>Both captains have to PM the referee which map they would like to ban out of the 3 maps within 90 seconds. Using the #multiplayer channel is strongly discouraged during this time.</p>
+            </li>
+            <li>
+              <p>If both teams ban different maps, the 3rd map is played.</p>
+            </li>
+            <li>
+              <p>If both teams ban the same map, the referee will do a !roll 2 to determine which map out of the remaining ones will be played, where 1 corresponds to the map listed higher on the mappool..</p>
+            </li>
+            <li>
+              <p>If the tiebreaker map ends in a draw, captains will roll again and the lower roll gets to choose another tiebreaker within the remaining 2 maps.</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+      
+    </section>
   </div>
 </template>
