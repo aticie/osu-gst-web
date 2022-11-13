0fd1ff3920d7003275c434baf43d81a86d37c487
Sibyl <metinkorkmaz417@gmail.com>
2022-11-13 11:56:16 +0300

    move avatar_url from teamBase to playerlessteam

--- frontend/src/Models/Team.ts
+++ frontend/src/Models/Team.ts
@@ -2,12 +2,12 @@ import { TeamlessUser } from "./User"
 
 export interface TeamBase {
   title: string,
-  avatar_url: string | undefined
 }
 
 export interface TeamCreate extends TeamBase {}
 export interface PlayerlessTeam extends TeamBase {
   team_hash: string
+  avatar_url: string | undefined
 }
 
 export interface Team extends PlayerlessTeam {
