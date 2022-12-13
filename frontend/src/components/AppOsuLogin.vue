c8dd1d23c642d5b08ee3489d0e0d6b89cb368ff2
Sibyl <metinkorkmaz417@gmail.com>
2022-12-13 13:06:06 +0300

    delete unused imports

--- frontend/src/components/AppOsuLogin.vue
+++ frontend/src/components/AppOsuLogin.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { toDiscordAuth, toOsuAuth } from "../auth";
+import { toOsuAuth } from "../auth";
 import { useUserStore } from "../store";
 import TeamInvites from "./teams/TeamInvites.vue";
 
 import axios from "axios";
-import { Danger } from "./icons";
 import { User } from "../models/User";
 
 const userStore = useUserStore();
@@ -26,12 +25,6 @@ try {
 
       <img :src="userStore.user.osu_avatar_url" alt="user avatar" class="h-10 rounded-full" />
     </div>
-
-    <!-- <button v-if="!userStore.user.discord_id" @click="toDiscordAuth"
-      class="flex-center flex-col 2xl:flex-row gap-2 text-yellow-400 bg-dark p-2 whitespace-normal">
-      <Danger />
-      <p>PLEASE VERIFY YOUR DISCORD TO PARTICIPATE</p>
-    </button> -->
 
     <TeamInvites v-if="!userStore.user.team && userStore.user.discord_id" />
   </div>
